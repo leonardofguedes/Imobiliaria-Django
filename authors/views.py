632c18f6a58698f1e455c90df60018a211e93521
leonardofguedes <leonardo.fguedes@gmail.com>
2022-07-02 11:33:03 -0300

    Criando o formulário de registro do usuário, aperfeiçoando o método view e seu template.

--- authors/views.py
+++ authors/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .forms import RegisterForm
+
 
 def register_view(request):
-    return render(request, 'authors/pages/register_view.html')
+    if request.Post:
+        form = RegisterForm(request.POST)
+    else:
+        form = RegisterForm()
+    return render(request, 'authors/pages/register_view.html',
+                  {'form': form,
+                   })
