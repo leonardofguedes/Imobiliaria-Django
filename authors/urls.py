9ba6e74607431f7cac9274c1e46deb0d343cae62
leonardofguedes <leonardo.fguedes@gmail.com>
2022-07-02 11:55:58 -0300

    Criando um novo path para criação de usuário

--- authors/urls.py
+++ authors/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'authors'
 
 urlpatterns = [
     path('register/', views.register_view, name='register'),
+    path('create/', views.register_create, name='create')
 ]
 
 
--- authors/views.py
+++ authors/views.py
@@ -1,12 +1,18 @@
+from django.http import Http404
 from django.shortcuts import render
 from .forms import RegisterForm
 
 
 def register_view(request):
-    if request.Post:
-        form = RegisterForm(request.POST)
-    else:
-        form = RegisterForm()
+    form = RegisterForm()
+    return render(request, 'authors/pages/register_view.html',
+                  {'form': form,
+                   })
+
+def register_create(request):
+    if not request.Post:
+        raise Http404()
+    form = RegisterForm(request.POST)
     return render(request, 'authors/pages/register_view.html',
                   {'form': form,
                    })
