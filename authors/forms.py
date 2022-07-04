3d15f4fca943f7238cf95d40e42d0d249c5878f2
leonardofguedes <leonardo.fguedes@gmail.com>
2022-07-04 14:55:04 -0300

    Refatorando o código de forms.py

--- authors/forms.py
+++ authors/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+import re
 
 
 def add_attr(field, attr_name, attr_new_val):
@@ -11,6 +13,18 @@ def add_placeholder(field, placeholder_val):
     """Método que adicionará o placeholder nos campos da Classe"""
     add_attr(field, 'placeholder', placeholder_val)
 
+
+def strong_password(password):
+    regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
+
+    if not regex.match(password):
+        raise ValidationError((
+            'Password must have at least one uppercase letter, '
+            'one lowercase letter and one number. The length should be '
+            'at least 8 characters.'
+        ),
+            code='invalid'
+        )
 
 class RegisterForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
@@ -33,7 +47,8 @@ class RegisterForm(forms.ModelForm):
             'Password must have at least one uppercase letter, '
             'one lowercase letter and one number. The length should be '
             'at least 8 characters.'
-        )
+        ),
+        validators=[strong_password]
     )
     password2 = forms.CharField(
         required=True,
@@ -76,3 +91,34 @@ class RegisterForm(forms.ModelForm):
                 'placeholder': 'Type your password here'
             })
         }
+
+    def clean_password(self):
+        data = self.cleaned_data.get('password')
+        if 'atenção' in data:
+            raise ValidationError(
+                        'Não digite %(param)s no campo password',
+                        code='invalid',
+                        params={'param': '"atenção"'}
+                    )
+        return data
+
+    def clean_first_name(self):
+        data = self.cleaned_data.get('first_name')
+        if 'John Doe' in data:
+            raise ValidationError(
+                            'Não digite %(value)s no campo first name',
+                            code='invalid',
+                            params={'value': '"John Doe"'}
+            )
+        return data
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        password2 = cleaned_data.get('password2')
+        if password != password2:
+                password_confirmation_error = ValidationError('Password and password2 must be equal', code='invalid')
+                raise ValidationError({
+                        'password': password_confirmation_error,
+                        'password2': [password_confirmation_error],
+                })
