21cccf0cdf0b173ecfe426405f989fcc71f43f9e
leonardofguedes <leonardo.fguedes@gmail.com>
2022-07-05 11:11:24 -0300

    Criando método para filtrar emails repetidos

--- authors/forms.py
+++ authors/forms.py
@@ -94,6 +94,16 @@ class RegisterForm(forms.ModelForm):
             'password',
         ]
 
+    def clean_email(self):
+        email = self.cleaned_data.get('email', '')
+        exists = User.objects.filter(email=email).exists()
+
+        if exists:
+            raise ValidationError(
+                'User e-mail is already in use', code='invalid',
+            )
+        return email
+
     def clean(self):
         cleaned_data = super().clean()
         password = cleaned_data.get('password')
