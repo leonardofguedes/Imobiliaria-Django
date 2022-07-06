e0a0a40017a28586e25cac159609c4b6b6220091
leonardofguedes <leonardo.fguedes@gmail.com>
2022-07-06 15:35:05 -0300

    Inserindo o logout

--- authors/urls.py
+++ authors/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('register/create/', views.register_create, name='register_create'),
     path('login/', views.login_view, name='login'),
     path('login/create', views.login_create, name='login_create'),
+    path('logout/', views.logout_view, name='logout')
 ]
 
 
--- authors/views.py
+++ authors/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render, redirect
 from .forms.register_form import RegisterForm
 from .forms.login import LoginForm
 from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 
 def register_view(request):
@@ -12,7 +14,6 @@ def register_view(request):
     return render(request, 'authors/pages/register_view.html', {
         'form': form,
         'form_action': reverse('authors:register_create'),
-
     })
 
 
@@ -34,6 +35,7 @@ def register_create(request):
         messages.success(request, 'Your user is create, please log in.')
 
         del(request.session['register_form_data'])
+        return redirect(reverse('authors:login'))
 
     return redirect('authors:register')
 
@@ -48,4 +50,35 @@ def login_view(request):
 
 
 def login_create(request):
-    return render(request, 'authors/pages/login.html')
+    if not request.POST:
+        raise Http404()
+
+    form = LoginForm(request.POST)
+    login_url = reverse('authors:login')
+
+    if form.is_valid():
+        authenticated_user = authenticate(
+            username=form.cleaned_data.get('username', ''),
+            password=form.cleaned_data.get('password', ''),
+        )
+
+        if authenticated_user is not None:
+            messages.success(request, 'Your are logged in.')
+            login(request, authenticated_user)
+        else:
+            messages.error(request, 'Invalid credentials')
+    else:
+        messages.error(request, 'Invalid username or password')
+
+    return redirect(login_url)
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def logout_view(request):
+    if not request.POST:
+        return redirect(reverse('authors:login'))
+
+    if request.POST.get('username') != request.user.username:
+        return redirect(reverse('authors:login'))
+
+    logout(request)
+    return redirect(reverse('authors:login'))
