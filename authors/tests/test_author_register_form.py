b1ba77d7cfdfbde3ab0f1bf33d61bcc6d992d269
leonardofguedes <leonardo.fguedes@gmail.com>
2022-07-06 13:18:17 -0300

    Consertando os testes conforme as alterações nas urls

--- authors/tests/test_author_register_form.py
+++ authors/tests/test_author_register_form.py
@@ -49,6 +49,7 @@ class AuthorRegisterFormUnitTest(TestCase):
         current = form[field].field.label
         self.assertEqual(current, needed)
 
+
 class AuthorRegisterFormIntegrationTest(DjangoTestCase):
     """Teste integrado"""
     def setUp(self, *args, **kwargs):
@@ -72,33 +73,30 @@ class AuthorRegisterFormIntegrationTest(DjangoTestCase):
     ])
     def test_fields_cannot_be_empty(self, field, msg):
         self.form_data[field] = ''
-        url = reverse('authors:create')
+        url = reverse('authors:register_create')
         response = self.client.post(url, data=self.form_data, follow=True)
         self.assertIn(msg, response.content.decode('utf-8'))
         self.assertIn(msg, response.context['form'].errors.get(field))
 
     def test_username_field_min_length_should_be_4(self):
         self.form_data['username'] = 'joa'
-        url = reverse('authors:create')
+        url = reverse('authors:register_create')
         response = self.client.post(url, data=self.form_data, follow=True)
-
         msg = 'Username must have at least 4 characters'
         self.assertIn(msg, response.content.decode('utf-8'))
         self.assertIn(msg, response.context['form'].errors.get('username'))
 
     def test_username_field_max_length_should_be_150(self):
         self.form_data['username'] = 'A' * 151
-        url = reverse('authors:create')
+        url = reverse('authors:register_create')
         response = self.client.post(url, data=self.form_data, follow=True)
-
         msg = 'Username must have less than 150 characters'
-
         self.assertIn(msg, response.context['form'].errors.get('username'))
         self.assertIn(msg, response.content.decode('utf-8'))
 
     def test_password_field_have_lower_upper_case_letters_and_numbers(self):
         self.form_data['password'] = 'abc123'
-        url = reverse('authors:create')
+        url = reverse('authors:register_create')
         response = self.client.post(url, data=self.form_data, follow=True)
         msg = (
             'Password must have at least one uppercase letter, '
@@ -108,30 +106,30 @@ class AuthorRegisterFormIntegrationTest(DjangoTestCase):
         self.assertIn(msg, response.context['form'].errors.get('password'))
         self.assertIn(msg, response.content.decode('utf-8'))
         self.form_data['password'] = '@A123abc123'
-        url = reverse('authors:create')
+        url = reverse('authors:register_create')
         response = self.client.post(url, data=self.form_data, follow=True)
         self.assertNotIn(msg, response.context['form'].errors.get('password'))
 
     def test_password_and_password_confirmation_are_equal(self):
         self.form_data['password'] = '@A123abc123'
         self.form_data['password2'] = '@A123abc1235'
-        url = reverse('authors:create')
+        url = reverse('authors:register_create')
         response = self.client.post(url, data=self.form_data, follow=True)
         msg = 'Password and password2 must be equal'
         self.assertIn(msg, response.context['form'].errors.get('password'))
         self.assertIn(msg, response.content.decode('utf-8'))
         self.form_data['password'] = '@A123abc123'
         self.form_data['password2'] = '@A123abc123'
-        url = reverse('authors:create')
+        url = reverse('authors:register_create')
 
     def test_send_get_request_to_registration_create_view_returns_404(self):
-        url = reverse('authors:create')
+        url = reverse('authors:register_create')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
 
     def test_author_created_can_login(self):
         """Testando o login do usuário"""
-        url = reverse('authors:create')
+        url = reverse('authors:register_create')
         self.form_data.update({
             'username': 'testusername',
             'password': 'Teste123456',
