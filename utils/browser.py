2a4e6f8f603f3e56a85553991e8a45777bcfeecf
leonardofguedes <leonardo.fguedes@gmail.com>
2022-07-08 12:00:09 -0300

    Aperfeiçoando browser.py

--- utils/browser.py
+++ utils/browser.py
@@ -1,5 +1,26 @@
 from pathlib import Path
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from time import sleep
 
 ROOT_PATH = Path(__file__).parent.parent
 CHROMEDRIVER_NAME = 'chromedriver.exe'
 CHROMEDRIVER_PATH = ROOT_PATH / 'bin' / CHROMEDRIVER_NAME
+
+def make_chrome_browser(*options):
+    chrome_options = webdriver.ChromeOptions()
+
+    if options is not None:
+        for option in options:
+            chrome_options.add_argument(option)
+
+    chrome_service = Service(executable_path=CHROMEDRIVER_PATH)
+    browser = webdriver.Chrome(service=chrome_service, options=chrome_options)
+    return browser
+
+#--headless é uma opção do driver para que o navegador não seja aberto
+if __name__ == '__main__':
+    browser = make_chrome_browser('--headless')
+    browser.get('http://www.udemy.com/')
+    sleep(5)
+    browser.quit()
