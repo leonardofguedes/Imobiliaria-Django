8acd7a7850c15b26cc7906a4bd04a3993d5f332d
leonardofguedes <leonardo.fguedes@gmail.com>
2022-07-11 15:37:10 -0300

    Criando uma Super Classe para ser usada pelas outras

--- tests/functional_tests/test_home_page.py
+++ tests/functional_tests/test_home_page.py
@@ -4,15 +4,21 @@ import time
 from selenium.webdriver.common.by import By
 
 
-class HomePageFunctionalTest(StaticLiveServerTestCase):
+class BaseFunctionalTest(StaticLiveServerTestCase):
+    def setUp(self) -> None:
+        self.browser = make_chrome_browser()
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+        return super().tearDown()
+
     def sleep(self, seconds=5):
         """Apenas para que dê tempo e possamos ver o teste sendo feito no browser"""
         time.sleep(seconds)
 
+class HomePageFunctionalTest(BaseFunctionalTest):
     def test_the_test(self):
-        browser = make_chrome_browser()
-        browser.get(self.live_server_url)
-        self.sleep(6)
-        body = browser.find_element(By.TAG_NAME, 'body')
+        self.browser.get(self.live_server_url)
+        body = self.browser.find_element(By.TAG_NAME, 'body')
         self.assertIn('Nenhum imóvel cadastrado ainda', body.text)
-        browser.quit()
