c64109d79e8dd3018838ce41b9bbb46a3b986543
leonardofguedes <leonardo.fguedes@gmail.com>
2022-07-11 17:34:12 -0300

    Inserindo o método make_home_in_batch nos testes

--- tests/functional_tests/test_home_page.py
+++ tests/functional_tests/test_home_page.py
@@ -1,9 +1,12 @@
 from selenium.webdriver.common.by import By
 from .base_test import BaseFunctionalTest
+from unittest.mock import patch
 
 
 class HomePageFunctionalTest(BaseFunctionalTest):
+    @patch('rental.views.PER_PAGE', new=2)
     def test_home_page_without_houses_message(self):
+        self.make_home_in_batch(qtd=0)
         self.browser.get(self.live_server_url)
         body = self.browser.find_element(By.TAG_NAME, 'body')
         self.assertIn('Nenhum imóvel cadastrado ainda', body.text)
