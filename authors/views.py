ce4ec3f12f062b82c586b077b136c9da7e0910a2
leonardofguedes <leonardo.fguedes@gmail.com>
2022-07-12 17:40:58 -0300

    Alterando create p/ created mensagem do usuario criado

--- authors/views.py
+++ authors/views.py
@@ -32,7 +32,7 @@ def register_create(request):
         user = form.save(commit=False)
         user.set_password(user.password)
         user.save()
-        messages.success(request, 'Your user is create, please log in.')
+        messages.success(request, 'Your user is created, please log in.')
 
         del(request.session['register_form_data'])
         return redirect(reverse('authors:login'))
