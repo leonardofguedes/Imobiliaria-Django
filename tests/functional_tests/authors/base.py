34f496bcf688bfd18fcafaf9f4ad4f5298ce997c
leonardofguedes <leonardo.fguedes@gmail.com>
2022-07-12 17:52:17 -0300

    Tirando o método get_by_placeholder e colocando em base.py

--- tests/functional_tests/authors/base.py
+++ tests/functional_tests/authors/base.py
@@ -1,6 +1,7 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from utils.browser import make_chrome_browser
 import time
+from selenium.webdriver.common.by import By
 
 
 class AuthorsBaseTest(StaticLiveServerTestCase):
@@ -14,3 +15,8 @@ class AuthorsBaseTest(StaticLiveServerTestCase):
 
     def sleep(self, qtd=10):
         time.sleep(qtd)
+
+    def get_by_placeholder(self, web_element, placeholder):
+        return web_element.find_element(
+            By.XPATH, f'//input[@placeholder="{placeholder}"]'
+        )
--- tests/functional_tests/authors/test_authors_register.py
+++ tests/functional_tests/authors/test_authors_register.py
@@ -1,14 +1,11 @@
 from .base import AuthorsBaseTest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import pytest
 
 
+@pytest.mark.functional_test
 class AuthorsRegisterTest(AuthorsBaseTest):
-    def get_by_placeholder(self, web_element, placeholder):
-        return web_element.find_element(
-            By.XPATH, f'//input[@placeholder="{placeholder}"]'
-        )
-
     def fill_forms_with_data(self, form):
         fields = form.find_elements(By.TAG_NAME, 'input')
         for field in fields:
