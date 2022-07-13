92920c605d1b545da62ccbd5e95eb29f7c1ac5ee
leonardofguedes <leonardo.fguedes@gmail.com>
2022-07-13 11:42:19 -0300

    Criando dashboard para os corretores

--- authors/views.py
+++ authors/views.py
@@ -54,7 +54,6 @@ def login_create(request):
         raise Http404()
 
     form = LoginForm(request.POST)
-    login_url = reverse('authors:login')
 
     if form.is_valid():
         authenticated_user = authenticate(
@@ -70,7 +69,7 @@ def login_create(request):
     else:
         messages.error(request, 'Invalid username or password')
 
-    return redirect(login_url)
+    return redirect(reverse('authors:dashboard'))
 
 @login_required(login_url='authors:login', redirect_field_name='next')
 def logout_view(request):
@@ -85,3 +84,7 @@ def logout_view(request):
     messages.success(request, 'Logged out successfully')
     logout(request)
     return redirect(reverse('authors:login'))
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def dashboard(request):
+    return render(request, 'authors/pages/dashboard.html')
