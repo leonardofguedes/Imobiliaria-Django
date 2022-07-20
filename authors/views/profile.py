0b459b990279612a179b2ef413c9a7dfc3663332
leonardofguedes <leonardo.fguedes@gmail.com>
2022-07-20 19:07:59 -0300

    Atualizando profile.py

--- authors/views/profile.py
+++ authors/views/profile.py
@@ -1,8 +1,17 @@
 from django.views.generic import TemplateView
+from django.shortcuts import get_object_or_404
+from authors.models import Profile
 
 class ProfileView(TemplateView):
     template_name = 'authors/pages;profile.hhtml'
 
     def get(self, request, *args, **kwargs):
         context = self.get_context_data(**kwargs)
-        return self.render_to_response(context)
+        profile_id = context.get('id')
+        profile = get_object_or_404(Profile.objects.filter(
+            pk = profile_id
+        ), pk=profile_id)
+        return self.render_to_response({
+            **context,
+        'profile': profile,
+        })
