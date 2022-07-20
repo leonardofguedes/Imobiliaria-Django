e15860d068249698f985d85600c0b099126abee7
leonardofguedes <leonardo.fguedes@gmail.com>
2022-07-20 18:12:03 -0300

    Criando model de Profile

--- authors/models.py
+++ authors/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    author = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(default='', blank=True)
+    phoneNumberRegex = RegexValidator(regex=r"^\+?1?\d{8,15}$")
+    phoneNumber = models.CharField(validators=[phoneNumberRegex], max_length=16, unique=True)
