583ab25f1ae6fecfd3b21cef5afc1ea3eb4ef2c4
leonardofguedes <leonardo.fguedes@gmail.com>
2022-07-21 10:02:51 -0300

    Criando admin.py

--- tag/admin.py
+++ tag/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Tag
 
-# Register your models here.
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name', 'slug',
+    list_display_links = 'id', 'slug',
+    search_fields = 'id', 'slug', 'name',
+    list_per_page = 10
+    list_editable = 'name',
+    ordering = '-id',
+    prepopulated_fields = {
+        'slug': ('name',),
+    }
