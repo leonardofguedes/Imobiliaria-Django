987c42858b43acf95cea876289cf8a5fe9dc4511
leonardofguedes <leonardo.fguedes@gmail.com>
2022-07-21 10:02:35 -0300

    Criando models.py

--- tag/models.py
+++ tag/models.py
@@ -1,3 +1,23 @@
+import string
+from random import SystemRandom
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            rand_letters = ''.join(
+                SystemRandom().choices(
+                    string.ascii_letters + string.digits,
+                    k=5,
+                )
+            )
+            self.slug = slugify(f'{self.name}-{rand_letters}')
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
