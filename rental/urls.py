4d85243f68e499572268bd6f54e5969b6b10f2e8
leonardofguedes <leonardo.fguedes@gmail.com>
2022-08-17 10:17:59 -0300

    Refatorando

--- rental/urls.py
+++ rental/urls.py
@@ -5,8 +5,7 @@ from . import views
 #app_name = 'rental'
 
 urlpatterns = [
-    path('', views.ListViewBase.as_view(), name="imobiliaria-home"),
-    path('home/', views.HomeListView.as_view(), name="home"),
+    path('', views.HomeListView.as_view(), name="imobiliaria-home"),
     path('imovel/search/', views.ListViewSearch.as_view(), name='search'),
     path('imovel/<int:pk>/', views.Detail.as_view(), name='imoveis-house'),
 ]
